63d8c513b49decbef58ee42eeb14f234ec8e4a2e
unknown <alan_pomerantz@hotmail.com>
2014-06-22 20:24:54 -0300

    Started k=3

--- tpe2/src/encription.c
+++ tpe2/src/encription.c
@@ -8,7 +8,8 @@ typedef unsigned char byte;
 void k_2_encode(image_t**, image_t*, int);
 void k_3_encode(image_t**, image_t*, int);
 void k_4_encode(image_t**, image_t*, int);
-int checkLinealDependency(int, int, byte, byte, image_t**, int);
+int checkLinealDependencyK2(int, int, byte, byte, image_t**, int);
+int checkLinealDependencyK3(int, int, byte, byte, byte, image_t**, int);
 char * byte_to_binary(int);
 
 char * byte_to_binary(int x) {
@@ -55,8 +56,13 @@ int encode(image_t* secret, int k, int n) {
     shadows[1] = read_image("src\\encode2.bmp");
     shadows[2] = read_image("src\\encode3.bmp");
     shadows[3] = read_image("src\\encode4.bmp");
-
+	
+	
     if (k == 2) {
+		if (secret->size % 2 != 0) {
+			printf("Not Supported !! '\n'");
+			return -1;
+		}
         k_2_encode(shadows, secret, shadow_count);
         shadows[0]->id = "src\\encode1shadow.bmp";
         shadows[1]->id = "src\\encode2shadow.bmp";
@@ -64,13 +70,17 @@ int encode(image_t* secret, int k, int n) {
         write_image(shadows[1]);
     }
     if (k == 3) {
-        k_3_encode(shadows, secret, shadow_count);
+        if (secret->size % 3 != 0) {
+			printf("Not Supported !! '\n'");
+			return -1;
+		}
+		k_3_encode(shadows, secret, shadow_count);
     }
     if (k == 4) {
         printf("Not supported");
+		return 1;
     }
-
-    return 1;
+    return -1;
 }
 
 void k_2_encode(image_t** shadows, image_t* secret, int shadow_count) {
@@ -78,7 +88,7 @@ void k_2_encode(image_t** shadows, image_t* secret, int shadow_count) {
     //int **coefficient_matrix = calloc(sizeof(int*),shadow_count);
     int decal = 4;
     int img_size;
-	int count = 0;
+    int count = 0;
     byte secret_first_byte;
     byte shadow_first_byte;
     byte secret_second_byte;
@@ -86,23 +96,19 @@ void k_2_encode(image_t** shadows, image_t* secret, int shadow_count) {
     byte decal_first_shadow_byte;
     byte decal_second_shadow_byte;
     int result;
-    if (secret->size % 2 != 0) {
-        printf("Not Supported !! '\n'");
-        return;
-    }
-
+    
     img_size = secret->size - secret->offset;
     for (index = 0; index < img_size; index += 2) {
         secret_first_byte = secret->bytes[index];
         secret_second_byte = secret->bytes[index + 1];
-		for (current_shadow = 0; current_shadow < shadow_count; current_shadow++) {
+        for (current_shadow = 0; current_shadow < shadow_count; current_shadow++) {
             shadow_first_byte = shadows[current_shadow]->bytes[index];
             decal_first_shadow_byte = shadow_first_byte >> decal;
             shadow_second_byte = shadows[current_shadow]->bytes[index + 1];
             decal_second_shadow_byte = shadow_second_byte >> decal;
-            while (checkLinealDependency(index, current_shadow, decal_first_shadow_byte, decal_second_shadow_byte, shadows, shadow_count) == 1) {
-				count++;
-				decal_first_shadow_byte = (decal_first_shadow_byte + 1) % 15;
+            while (checkLinealDependencyK2(index, current_shadow, decal_first_shadow_byte, decal_second_shadow_byte, shadows, shadow_count) == 1) {
+                count++;
+                decal_first_shadow_byte = (decal_first_shadow_byte + 1) % 15;
             }
             result = (decal_first_shadow_byte * secret_first_byte + decal_second_shadow_byte * secret_second_byte) % 251;
             shadows[current_shadow]->bytes[index] &= 0b11110000;
@@ -117,9 +123,8 @@ void k_2_encode(image_t** shadows, image_t* secret, int shadow_count) {
                 shadows[current_shadow]->bytes[index + 1] &= 0b11110111;
             }
         }
-
     }
-	printf("cambio %d '\n'",count);
+    printf("cambio %d '\n'", count);
     return;
 }
 
@@ -136,7 +141,72 @@ int calculateAuth(int a1, int a2) {
     return xorFromHashWrapper(hash, strlen(hash));
 }
 
+int calculateAuthK3(int a1, int a2, int a3) {
+    char hashValue[25]; // 3 BYTES . K=3 + 1 for '/o'
+    char* a1_binary = byte_to_binary(a1);
+    strcpy(hashValue, a1_binary);
+    a2 = a2 << 1;
+    char* a2_binary = byte_to_binary(a2);
+    strcat(hashValue, a2_binary);
+	a3 = a3 << 1;
+	char* a3_binary = byte_to_binary(a3);
+    strcat(hashValue, a3_binary);
+	// unsigned char* md = malloc(MD5_DIGGEST_LENGTH);
+    // unsigned char* hash = MD5(hashValue,strlen(hashValue),md);
+    unsigned char hash[] = "9214c48d83c004bf0afc2f0b8a02685d";
+    return xorFromHashWrapper(hash, strlen(hash));
+}
+
+
 void k_3_encode(image_t** shadows, image_t* secret, int shadow_count) {
+    int index, current_shadow, j, authentication_bit = 0;
+    //int **coefficient_matrix = calloc(sizeof(int*),shadow_count);
+    int decal = 5;
+    int img_size;
+    int count = 0;
+    byte secret_first_byte;
+    byte shadow_first_byte;
+    byte secret_second_byte;
+    byte shadow_second_byte;
+    byte secret_third_byte;
+    byte shadow_third_byte;
+    byte decal_first_shadow_byte;
+    byte decal_second_shadow_byte;
+    byte decal_third_shadow_byte;
+    int result;
+    
+    img_size = secret->size - secret->offset;
+    for (index = 0; index < img_size; index += 3) {
+        secret_first_byte = secret->bytes[index];
+        secret_second_byte = secret->bytes[index + 1];
+        secret_third_byte = secret->bytes[index + 2];
+        for (current_shadow = 0; current_shadow < shadow_count; current_shadow++) {
+            shadow_first_byte = shadows[current_shadow]->bytes[index];
+            decal_first_shadow_byte = shadow_first_byte >> decal;
+            shadow_second_byte = shadows[current_shadow]->bytes[index + 1];
+            decal_second_shadow_byte = shadow_second_byte >> decal;
+            shadow_third_byte = shadows[current_shadow]->bytes[index + 2];
+            decal_third_shadow_byte = shadow_third_byte >> decal;
+            while (checkLinealDependencyK3(index, current_shadow, decal_first_shadow_byte, decal_second_shadow_byte, decal_third_shadow_byte, shadows, shadow_count) == 1) {
+                count++;
+                decal_first_shadow_byte = (decal_first_shadow_byte + 1) % 15;
+            }
+            result = (decal_first_shadow_byte * secret_first_byte + decal_second_shadow_byte * secret_second_byte + decal_third_shadow_byte * secret_third_byte) % 251;
+            shadows[current_shadow]->bytes[index] &= 0b11111000;
+            shadows[current_shadow]->bytes[index] |= result >> 5;
+            shadows[current_shadow]->bytes[index + 1] &= 0b11111000; //ONLY 3 . 1 FOR AUTHENTICATION BIT !!
+            shadows[current_shadow]->bytes[index + 1] |= (result & 0b00011100) >> 2;
+            shadows[current_shadow]->bytes[index + 2] &= 0b11111000;
+            shadows[current_shadow]->bytes[index + 2] |= (result & 0b00000011);
+            authentication_bit = calculateAuthK3(shadows[current_shadow]->bytes[index], shadows[current_shadow]->bytes[index + 1], shadows[current_shadow]->bytes[index + 2]);
+            // authentication_bit = 1;
+            if (authentication_bit == 1) {
+                // shadows[current_shadow]->bytes[index + 1] |= 0b00001000;
+            } else {
+                // shadows[current_shadow]->bytes[index + 1] &= 0b11110111;
+            }
+        }
+    }
     return;
 }
 
@@ -156,7 +226,11 @@ int isDependent(int a, int b) {
     return ((a % b == 0) || (b % a == 0));
 }
 
-int checkLinealDependency(int index, int current_shadow, byte first_byte, byte second_byte, image_t** shadows, int n) {
+int checkLinealDependencyK3(int index, int current_shadow, byte first_byte, byte second_byte, byte third_byte, image_t** shadows, int n) {
+	return 0;
+}
+
+int checkLinealDependencyK2(int index, int current_shadow, byte first_byte, byte second_byte, image_t** shadows, int n) {
     int i;
     double first_multiplier, second_multiplier;
     double epsilon = 0.0001;
@@ -169,9 +243,8 @@ int checkLinealDependency(int index, int current_shadow, byte first_byte, byte s
                     if (first_multiplier - second_multiplier < epsilon)
                         return 1;
                 }
-            } 
+            }
         }
     }
-
     return 0;
 }
