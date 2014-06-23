e2f78c73242cd78faef1fb0eb3fa16ecdc65c3d1
Juan Jose Marinelli <juanjosemarinelli@gmail.com>
2014-06-22 21:55:39 -0300

    cambios

--- tpe2/src/decrypt.c
+++ tpe2/src/decrypt.c
@@ -11,6 +11,7 @@
 
 void decrypt_bytes_k2(image_t * secret_image, image_t ** images, int block_position, int image_count);
 void decrypt_bytes_k3(image_t * secret_image, image_t ** images, int block_position, int image_count);
+void recover_block3(image_t * secret_image, image_t ** images, int block_position, int image_count);
 
 image_t * decrypt(const char * directory, int k) {
 	struct dirent *p_dirent;
@@ -46,7 +47,7 @@ image_t * decrypt(const char * directory, int k) {
 
 	void (*decrypt_bytes[2]) (image_t * secret, image_t ** images, int index, int image_qty);
 	decrypt_bytes[0] = &decrypt_bytes_k2;
-	decrypt_bytes[1] = &decrypt_bytes_k3;
+	decrypt_bytes[1] = &recover_block3;
 
   for (i = 0; i < secret_image->size - secret_image->offset; i += k) {
 			if ((*decrypt_bytes[k - 2])) {
@@ -138,4 +139,95 @@ void decrypt_bytes_k3(image_t * secret_image, image_t ** images, int index, int 
     secret_image->bytes[index] = x;
     secret_image->bytes[index + 1] = y;
     secret_image->bytes[index + 2] = z;
+}
+
+
+void recover_block3(image_t * secret_image, image_t ** images, int block_position, int image_count) {
+    int coefficients[image_count][4];
+    int i, j;
+    for (i = 0; i < image_count; i++) {
+        coefficients[i][0] = images[i]->bytes[block_position] >> 3;
+        coefficients[i][1] = images[i]->bytes[block_position + 1] >> 3;
+        // Shift 6 times to take parity bit out
+        coefficients[i][2] = images[i]->bytes[block_position + 2] >> 3;
+        coefficients[i][3] = ((images[i]->bytes[block_position] & 7) << 5) | ((images[i]->bytes[block_position + 1] & 7) << 2) | (images[i]->bytes[block_position + 2] & 3);
+    }
+
+
+
+    // Take the first item of every equation to 1 to solve.
+    for (i = 0; i < image_count; i++) {
+        unsigned char first_mod_inverse = inverse(coefficients[i][0]);
+        for (j = 0; j < 4; j++) {
+            coefficients[i][j] = ((int)coefficients[i][j] * first_mod_inverse) % 251;
+        }
+    }
+
+    // Substract first row from every other
+    for (i = 1; i < image_count; i++) {
+        for (j = 0; j < 4; j++) {
+            int result = ((int)coefficients[i][j] - coefficients[0][j]);
+            coefficients[i][j] = (result < 0) ? result + 251 : result;
+        }
+    }
+
+    unsigned char aux;
+    if (coefficients[1][1] == 0) {
+        for (j = 0; j < 4; j++) {
+            aux = coefficients[1][j];
+            coefficients[1][j] = coefficients[2][j];
+            coefficients[2][j] = aux;
+        }
+    } else {
+        if (coefficients[2][1] != 0) {
+            // Take the second item of every equation below the first one to 1 to solve.
+            for (i = 1; i < image_count; i++) {
+                unsigned char second_mod_inverse = inverse(coefficients[i][1]);
+                for (j = 1; j < 4; j++) {
+                    coefficients[i][j] = ((int)coefficients[i][j] * second_mod_inverse) % 251;
+                }
+            }
+
+            // Substract second row from every other
+            // TODO: Modularize this
+            for (i = 2; i < image_count; i++) {
+                for (j = 0; j < 4; j++) {
+                    int result = ((int)coefficients[i][j] - coefficients[1][j]);
+                    coefficients[i][j] = (result < 0) ? result + 251 : result;
+                }
+            }
+        }
+    }
+
+    if (coefficients[1][0] != 0 || coefficients[2][0] != 0 || coefficients[2][1] != 0 || coefficients[1][1] == 0 || coefficients[2][2] == 0) {
+        printf("Something went wrong with the equations!\n");
+        printf("%d %d %d\n%d %d %d\n", coefficients[1][0], coefficients[1][1], coefficients[1][2], coefficients[2][0], coefficients[2][1], coefficients[2][2]);
+    }
+
+    // Use the first three equations to solve the system.
+		int z = (coefficients[2][3] * inverse(coefficients[2][2])) % 251;
+    int y = (coefficients[1][3] - (coefficients[1][2] * z)) % 251;
+    int x = (coefficients[0][3] - (coefficients[0][2] * z) - (coefficients[0][1] * y)) % 251;
+
+		/*
+		int z = ((int)coefficients[2][3] * inverse((int)coefficients[2][2])) % 251;
+    int y = ((int)coefficients[1][3] - ((int)coefficients[1][2] * z)) % 251;
+    int x = ((int)coefficients[0][3] - ((int)coefficients[0][2] * z) - ((int)coefficients[0][1] * y)) % 251;
+		if (z < 0)
+			z += 251;
+		if (y < 0)
+			y += 251;
+		if (x < 0)
+			x += 251;
+
+		if (x < 0 || x > 250)
+			printf("x");
+		if (y < 0 || y > 250)
+			printf("y");
+		if (z < 0 || z > 250)
+			printf("z"); */
+
+    secret_image->bytes[block_position] = (unsigned char) x;
+    secret_image->bytes[block_position + 1] = (unsigned char) y;
+    secret_image->bytes[block_position + 2] = (unsigned char) z;
 }
